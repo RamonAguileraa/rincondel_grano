8fe8c5a3b321b705593a987509e8f9f91c76a242
RamonAguileraa <126223036+RamonAguileraa@users.noreply.github.com>
2024-09-18 14:47:26 -0600

    columns changed

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -8,10 +8,22 @@ export default function Hero() {
       style={{ backgroundImage: 'url(/hero.jpeg)', backgroundColor: '#36413E' }}
     >
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative z-10 text-center md:text-left px-4 md:px-8">
-        <h2 className="text-5xl font-bold mb-4 text-[#FCFDAF] leading-tight animate__animated animate__fadeIn animate__delay-1s">
-          Bienvenido a <br /> El Rincón del Grano
+       <div className="hidden md:block mt-8 animate__animated animate__fadeIn animate__delay-3s">
+          <Image
+            src="/texto.png"
+            alt="Texto de bienvenida"
+            width={400}
+            height={400}
+            className="mx-auto rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"
+          />
+        </div>
+      <div  className=" grid-cols-2 relative  z-10 text-center md:text-left px-4 md:px-8">
+
+        <h2 className="text-5xl font-bold mb-4 text-[#758E4F] leading-tight animate__animated animate__fadeIn animate__delay-1s">
+          Bienvenido  <span className='text-[#FCFDAF]'>a</span><br />
+         <span className='text-[#FCFDAF]'>El</span> <span className='text-[#FCFDAF]'>Rincon </span> <span>del</span> <span className='text-[#758E4F]'>grano</span>
         </h2>
+
         <p className="text-xl mb-6 text-[#EFE9F4] animate__animated animate__fadeIn animate__delay-2s">Disfruta de una experiencia culinaria única con los mejores granos de café y platos deliciosos.</p>
         <a 
           href="#reservations" 
@@ -19,16 +31,7 @@ export default function Hero() {
         >
           Haz tu Reserva
         </a>
-        <div className="hidden md:block mt-8 animate__animated animate__fadeIn animate__delay-3s">
-          <Image
-            src="/texto.png"
-            alt="Texto de bienvenida"
-            width={400}
-            height={400}
-            objectFit="cover"
-            className="mx-auto rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"
-          />
-        </div>
+       
       </div>
     </section>
   );
