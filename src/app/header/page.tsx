09aef2ac485e5db63bac3928b5661697646876b2
RamonAguileraa <ra16monaagui16lera@gmail.com>
2024-09-18 19:11:47 -0600

    font changes in navbar

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -2,8 +2,8 @@ export default function Header() {
     return (
       <header className="bg-[#36413E] text-[#FCFDAF] py-4 shadow-md">
         <nav className="container mx-auto flex items-center justify-between">
-          <a href="#" className="text-3xl font-bold">El Rincón del Grano</a>
-          <ul className="flex space-x-4">
+          <a href="#" className="text-3xl italic font-serif">El Rincón del Grano</a>
+          <ul className="flex space-x-4 font-serif text-lg">
             <li>
               <a href="#hero" className="hover:text-[#758E4F] transition-colors duration-300">Inicio</a>
             </li>
